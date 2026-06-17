39d03ea226a9c641d36737182bf986a6fab8fd9b
Code-withAnsh <anshs9333@gmail.com>
2026-06-17 10:18:09 +0530

    Add Day 08 - pattern problem

---
+++ Day 08/pattern3.js
@@ -0,0 +1,10 @@
+//right angle triangle
+let prompt = require('prompt-sync')()
+let n = prompt('enter a number')
+for(let i = 1;i<=n;i++){
+    for(let j = 1; j<=i;j++){
+        process.stdout.write('* ')
+    }
+    console.log();
+    
+}
---
+++ Day 08/pattern5.js
@@ -0,0 +1,10 @@
+//inverted right angle triangle
+let prompt = require('prompt-sync')()
+let n = prompt('enter a number')
+for(let i = 1; i<=n;i++){
+    for(let j = 1; j<n-i+1;j++){
+        process.stdout.write('* ')
+    }
+    console.log();
+    
+}
